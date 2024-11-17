4a5929f14d0f9238b341405de9cfdb446c7ec9ed
hmooda11 <hmooda11@hotmail.com>
2024-11-17 16:53:17 +0100

    Assignment3-2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script outline to install and build kernel.
+# Script to install and build kernel.
 # Author: Siddhant Jajoo.
 
 set -e
@@ -15,10 +15,10 @@ CROSS_COMPILE=aarch64-none-linux-gnu-
 
 if [ $# -lt 1 ]
 then
-	echo "Using default directory ${OUTDIR} for output"
+    echo "Using default directory ${OUTDIR} for output"
 else
-	OUTDIR=$1
-	echo "Using passed directory ${OUTDIR} for output"
+    OUTDIR=$(realpath $1)
+    echo "Using passed directory ${OUTDIR} for output"
 fi
 
 mkdir -p ${OUTDIR}
@@ -26,15 +26,31 @@ mkdir -p ${OUTDIR}
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+    echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+    git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # Deep cleaning the kernel build tree and removing .config file with any existing configurations
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+
+    # Configure for the "virt" arm board that is simulated by QEMU
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+
+    # Building a kernel image for booting with QEMU since QEMU does not boot up automatically
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+
+    # Build any kernel modules (optional)
+    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+
+    # Build the device tree (optional)
+    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+
+    echo "Adding the Image in outdir"
+    cp ./arch/${ARCH}/boot/Image ${OUTDIR}/
 fi
 
 echo "Adding the Image in outdir"
@@ -43,38 +59,159 @@ echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+
+# Create necessary base directories
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # Configure busybox
+    make distclean
+    make defconfig
+    # Enable static build (optional)
+    # sed -i 's/.*CONFIG_STATIC.*/CONFIG_STATIC=y/' .config
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+
+
+cp /home/hmooda11/assignment-2-hmooda11/conf/username.txt /tmp/aeld/rootfs/conf
+
+# Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter" || true
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library" || true
 
-# TODO: Make device nodes
 
-# TODO: Clean and build the writer utility
+# Add library dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+echo "Sysroot is at ${SYSROOT}"
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+mkdir -p lib64
+sudo cp -L ${SYSROOT}/lib/ld-linux-aarch64.so.1 lib
+sudo cp -L ${SYSROOT}/lib64/libm.so.6 lib64
+sudo cp -L ${SYSROOT}/lib64/libresolv.so.2 lib64
+sudo cp -L ${SYSROOT}/lib64/libc.so.6 lib64
 
-# TODO: Chown the root directory
+# Make device nodes
+sudo mknod -m 666 dev/null c 1 3 || true
+sudo mknod -m 600 dev/console c 5 1 || true
 
-# TODO: Create initramfs.cpio.gz
+# Clean and build the writer utility
+cd $FINDER_APP_DIR/
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
+
+# Copy the finder related scripts and executables to the /home directory on the target rootfs
+cp $FINDER_APP_DIR/finder-test.sh ${OUTDIR}/rootfs/home
+cp -rL $FINDER_APP_DIR/conf ${OUTDIR}/rootfs/home
+cp $FINDER_APP_DIR/finder.sh ${OUTDIR}/rootfs/home
+cp $FINDER_APP_DIR/writer ${OUTDIR}/rootfs/home
+cp $FINDER_APP_DIR/autorun-qemu.sh ${OUTDIR}/rootfs/home
+
+cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home/writer
+cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home/finder.sh
+cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/finder-test.sh
+#sudo mkdir -p ${OUTDIR}/rootfs/home/conf/
+echo ${FINDER_APP_DIR}
+echo ${OUTDIR}
+ls ${OUTDIR}/rootfs/home/conf 
+ls -l /tmp/aeld/rootfs/home/conf
+
+
+
+cp /home/hmooda11/assignment-2-hmooda11/conf/username.txt /tmp/aeld/rootfs/conf/ || true
+
+
+echo "root/"
+ls -l /${OUTDIR}/rootfs/
+
+echo "root/etc"
+ls -l ${OUTDIR}/rootfs/etc
+
+echo "mkdir"
+sudo mkdir ${OUTDIR}/rootfs/etc/finder-app
+
+echo "mkdir"
+sudo mkdir ${OUTDIR}/rootfs/etc/finder-app/conf
+
+echo "etc"
+ls -l ${OUTDIR}/rootfs/etc
+sudo cp /home/hmooda11/assignment-2-hmooda11/conf/username.txt /tmp/aeld/rootfs/etc/finder-app/conf/ || true
+
+ls -l /tmp/aeld/rootfs/etc/finder-app/conf/ || true
+
+#cp /home/hmooda11/assignment-2-hmooda11/conf/assignment.txt ${OUTDIR}/rootfs/home/conf/assignment.txt
+#cp /home/hmooda11/assignment-2-hmooda11/autorun-qemu.sh ${OUTDIR}/rootfs/home/autorun-qemu.sh
+sudo cp /home/hmooda11/assignment-2-hmooda11/finder-app/conf/username.txt ${OUTDIR}/rootfs/etc/finder-app/conf/
+sudo cp /home/hmooda11/assignment-2-hmooda11/finder-app/conf/assignment.txt ${OUTDIR}/rootfs/etc/finder-app/conf/
+
+
+# Ensure the /home directory exists
+mkdir -p ${OUTDIR}/rootfs/home || true
+
+# Copy the finder related scripts and executables
+cp -a $FINDER_APP_DIR/finder-test.sh ${OUTDIR}/rootfs/home/ || true
+cp -a $FINDER_APP_DIR/finder.sh ${OUTDIR}/rootfs/home/
+cp -a $FINDER_APP_DIR/writer ${OUTDIR}/rootfs/home/ || true
+cp -a $FINDER_APP_DIR/autorun-qemu.sh ${OUTDIR}/rootfs/home/ || true
+cp -a $FINDER_APP_DIR/conf ${OUTDIR}/rootfs/home/ || true
+
+
+# Modify finder-test.sh to reference conf/assignment.txt
+sed -i 's|\.\./conf/assignment.txt|conf/assignment.txt|g' ${OUTDIR}/rootfs/home/finder-test.sh
+
+# Make scripts executable
+chmod +x ${OUTDIR}/rootfs/home/finder.sh
+chmod +x ${OUTDIR}/rootfs/home/finder-test.sh
+chmod +x ${OUTDIR}/rootfs/home/autorun-qemu.sh
+chmod +x ${OUTDIR}/rootfs/home/writer
+
+# Create the init script
+echo "Creating init script"
+cat << EOF > ${OUTDIR}/rootfs/init
+#!/bin/sh
+echo "Booting the system..."
+
+# Mount necessary filesystems
+mount -t proc none /proc
+mount -t sysfs none /sys
+mount -t devtmpfs none /dev
+
+# Run the autorun script
+cd /home
+./autorun-qemu.sh
+EOF
+
+# Make the init script executable
+chmod +x ${OUTDIR}/rootfs/init
+
+# Change ownership of the root directory
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root *
+
+# Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root 2> /dev/null | gzip -c > ${OUTDIR}/initramfs.cpio.gz
+
+echo "Success"
+
